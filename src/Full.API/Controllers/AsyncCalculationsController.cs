7f70ce55d9584d970e0bc841b25a909ab852fe3c
mikegsCoder <mikegsid@gmail.com>
2024-11-12 07:39:48 +0200

    Add AsyncCalculationsController and implemented add async into Full API

---
+++ src/Full.API/Controllers/AsyncCalculationsController.cs
@@ -0,0 +1,18 @@
+﻿namespace Full.API.Controllers;
+
+using Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("calc-async")]
+public class AsyncCalculationsController(ICalculator calculator) : ControllerBase
+{
+    private readonly ICalculator _calculator = calculator ?? throw new ArgumentNullException(nameof(calculator));
+
+    [HttpGet("add")]
+    public async Task<IActionResult> Add([FromQuery] int a, [FromQuery] int b, CancellationToken cancellationToken)
+    {
+        var result = await this._calculator.AddAsync(a, b, cancellationToken);
+        return this.Ok($"{a} + {b} = {result}");
+    }
+    // http://localhost:5000/calc-async/add?a=2&b=5 -> 2 + 5 = 7
+}
