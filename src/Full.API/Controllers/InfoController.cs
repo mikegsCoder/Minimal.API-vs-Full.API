040c17a26a1667158b7850d97ab4c6ae3b57c4da
mikegsCoder <mikegsid@gmail.com>
2024-11-15 07:24:03 +0200

    Add InfoController and request analysis end point

---
+++ src/Full.API/Controllers/InfoController.cs
@@ -0,0 +1,16 @@
+﻿namespace Full.API.Controllers;
+
+using Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("info")]
+public class InfoController : ControllerBase
+{
+    [HttpGet("request-analysis")]
+    public IActionResult RequestAnalysis()
+        => this.Ok($"Request Mehod: {this.Request.Method},\nRequest Path: {this.Request.Path.ToString().Trim()},\nContent Length: {this.Request.ContentLength ?? 0}");
+    // http://localhost:5000/info/request-analysis
+    // Request Mehod: GET,
+    // Request Path: /info/request-analysis,
+    // Content Length: 0
+}
