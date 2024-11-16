431d7b5f25be70bbe577e6d6d3fd1ba74430f902
mikegsCoder <mikegsid@gmail.com>
2024-11-16 07:05:33 +0200

    Add log example

--- src/Full.API/Controllers/InfoController.cs
+++ src/Full.API/Controllers/InfoController.cs
@@ -1,7 +1,9 @@
 ﻿namespace Full.API.Controllers;
 
 using Core.Contracts;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Core.Models.Log;
 
 [Route("info")]
 public class InfoController : ControllerBase
@@ -13,4 +15,20 @@ public class InfoController : ControllerBase
     // Request Mehod: GET,
     // Request Path: /info/request-analysis,
     // Content Length: 0
+
+    [HttpPost("log")]
+    public async Task<IActionResult> Log([FromBody] LogRequest request, [FromServices] ILogWriter logWriter, CancellationToken cancellationToken)
+    {
+        var logId = await logWriter.WriteAsync(request.Severity, request.Message, cancellationToken);
+        return this.Ok(logId);
+    }
+    // POST with Postman to http://localhost:5000/info/log
+    // Body JSON:
+    // {
+    //      "Message" : "my message",
+    //      "Severity" : 5
+    // }
+    // Response sattus: 200 OK
+    // Response body: "20241116-b89a0b73-9af8-40ec-b2de-a06eec2f7f9a"
+    // and .log file is created
 }
