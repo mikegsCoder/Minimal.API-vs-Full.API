8123ccb9fb3cea17f385fcfa3dfa992e68307e0b
mikegsCoder <mikegsid@gmail.com>
2024-11-17 15:26:17 +0200

    Add database constants

---
+++ src/Infrastructure/Constants/DatabaseConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure.Constants
+{
+    internal static class DatabaseConstants
+    {
+        // User model:
+        public const int User_Username_MaxLength = 15;
+        public const int User_FirstName_MaxLength = 15;
+        public const int User_LastName_MaxLength = 15;
+
+        // UserTask model:
+        public const int UserTask_Description_MaxLength = 75;
+
+        // Category model:
+        public const int Category_Name_MaxLength = 10;
+
+        // Status model:
+        public const int Status_Name_MaxLength = 10;
+    }
+}
