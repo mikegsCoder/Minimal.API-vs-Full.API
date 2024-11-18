7a36c83c7d4fce9cb2a81458b80c40724b2c9791
mikegsCoder <mikegsid@gmail.com>
2024-11-18 07:21:09 +0200

    Add Category db model

---
+++ src/Infrastructure/Data/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Infrastructure.Constants;
+
+namespace Infrastructure.Data.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Id = Guid.NewGuid().ToString();
+
+            Tasks = new List<UserTask>();
+        }
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(DatabaseConstants.Category_Name_MaxLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<UserTask> Tasks { get; set; }
+    }
+}
