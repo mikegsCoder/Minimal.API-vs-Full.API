6ecd8fe853191d9f3f6d3f9fc821404e9f7764e4
mikegsCoder <mikegsid@gmail.com>
2024-11-18 07:25:12 +0200

    Add UserTask db model

---
+++ src/Infrastructure/Data/Models/UserTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Constants;
+
+namespace Infrastructure.Data.Models
+{
+    public class UserTask
+    {
+        public UserTask()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public string Id { get; set; }
+
+        public Category Category { get; set; } = null!;
+
+        [Required]
+        public string CategoryId { get; set; } = null!;
+
+        public Status Status { get; set; } = null!;
+
+        [Required]
+        public string StatusId { get; set; } = null!;
+
+        public User User { get; set; } = null!;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DatabaseConstants.UserTask_Description_MaxLength)]
+        public string Description { get; set; } = null!;
+    }
+}
