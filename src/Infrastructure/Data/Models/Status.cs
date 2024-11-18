786faf88cadf98fddb3c41f600a87186730a6064
mikegsCoder <mikegsid@gmail.com>
2024-11-18 07:22:16 +0200

    Add Status db model

---
+++ src/Infrastructure/Data/Models/Status.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Infrastructure.Constants;
+
+namespace Infrastructure.Data.Models
+{
+    public class Status
+    {
+        public Status()
+        {
+            Id = Guid.NewGuid().ToString();
+
+            Tasks = new List<UserTask>();
+        }
+
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(DatabaseConstants.Status_Name_MaxLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<UserTask> Tasks { get; set; }
+    }
+}
