912de641344ff818dcbc1bcb87e0b62115a537bf
mikegsCoder <mikegsid@gmail.com>
2024-11-19 07:22:58 +0200

    Add User configuration

---
+++ src/Infrastructure/Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Security.Cryptography;
+using System.Text;
+using Infrastructure.Data.Models;
+
+namespace Infrastructure.Data.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(SeedUsers());
+        }
+
+        private IEnumerable<User> SeedUsers()
+        {
+            var peter = new User
+            {
+                Id = "90b21bc9-9062-4142-b3f9-774e6797e080",
+                Username = "peter123",
+                FirstName = "Peter",
+                LatName = "Petrov",
+            };
+
+            return new List<User>(){ peter };
+        }
+    }
+}
+
