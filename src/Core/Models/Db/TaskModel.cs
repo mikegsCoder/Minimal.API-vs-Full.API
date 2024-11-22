19e74aa94e389ed0dda55b2dc0d1057326db625d
mikegsCoder <mikegsid@gmail.com>
2024-11-22 07:28:07 +0200

    Add TaskModel

---
+++ src/Core/Models/Db/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Models.Db
+{
+    public class TaskModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Category { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
+
+        public string User { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+    }
+}
