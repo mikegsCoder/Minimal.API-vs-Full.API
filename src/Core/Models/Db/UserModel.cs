adddbba51999397d5f68716a134d59a059d1b80c
mikegsCoder <mikegsid@gmail.com>
2024-11-22 07:26:38 +0200

    Add User model

---
+++ src/Core/Models/Db/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Models.Db
+{
+    public class UserModel : UserBody
+    {
+        public string Id { get; set; } = null!;
+
+        public IEnumerable<TaskModel> Tasks { get; set; } = null!;
+    }
+}
---
+++ src/Core/Models/UserBody.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models
+{
+    public class UserBody
+    {
+        public string Username { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+    }
+}
