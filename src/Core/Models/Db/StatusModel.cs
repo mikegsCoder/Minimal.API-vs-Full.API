2489ce40e1c871c5d672cacc4e4a96ee075b4d9c
mikegsCoder <mikegsid@gmail.com>
2024-11-22 07:29:37 +0200

    Add StatusModel

---
+++ src/Core/Models/Db/StatusModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models.Db
+{
+    public class StatusModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public IEnumerable<TaskModel> Tasks { get; set; } = null!;
+    }
+}
