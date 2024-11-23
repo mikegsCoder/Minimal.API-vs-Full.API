d78c185856363d407159ffbe24b957031e48961a
mikegsCoder <mikegsid@gmail.com>
2024-11-23 10:10:43 +0200

    Add CategoryModel

---
+++ src/Core/Models/DbModels/CategoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models.DbModels
+{
+    public class CategoryModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public IEnumerable<TaskModel> Tasks { get; set; } = null!;
+    }
+}
