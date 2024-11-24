c08d5c97637b3cc354b4f4352d98b90b2f55c72b
mikegsCoder <mikegsid@gmail.com>
2024-11-24 09:23:47 +0200

    Add DbController

---
+++ src/Full.API/Controllers/DbController.cs
@@ -0,0 +1,65 @@
+﻿namespace Full.API.Controllers;
+
+using Core.Contracts;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+[Route("db")]
+public class DbController(IDbService dbService) : ControllerBase
+{
+    private readonly IDbService _db = dbService ?? throw new ArgumentNullException(nameof(dbService));
+
+    [HttpGet("users")]
+    public async Task<IActionResult> UserByUsername([FromQuery] string ? username, CancellationToken cancellationToken)
+    {
+        object result;
+
+        if (username != null)
+        {
+            result = await _db.GetUserByUsernameAsync(username, cancellationToken);
+        }
+        else
+        {
+            result = await _db.GetAllUsersAsync(cancellationToken);
+        }
+
+        return this.Ok(result);
+    }
+    // http://localhost:5000/db/users
+    // Response sattus: 200 OK
+    //[
+    //    {
+    //        "id": "90b21bc9-9062-4142-b3f9-774e6797e080",
+    //        "tasks": [
+    //            {
+    //                "id": "23bf9c00-056c-4c72-9fa7-396c28da66c7",
+    //                "category": "Personal",
+    //                "status": "In Progress",
+    //                "user": "peter123",
+    //                "description": "Create MinimalAPI demo project."
+    //            }
+    //        ],
+    //        "username": "peter123",
+    //        "firstName": "Peter",
+    //        "lastName": "Petrov"
+    //    }
+    //]
+    // http://localhost:5000/db/users?username=peter123
+    // Response sattus: 200 OK
+    //{
+    //    "id": "90b21bc9-9062-4142-b3f9-774e6797e080",
+    //    "tasks": [
+    //        {
+    //            "id": "23bf9c00-056c-4c72-9fa7-396c28da66c7",
+    //            "category": "Personal",
+    //            "status": "In Progress",
+    //            "user": "peter123",
+    //            "description": "Create MinimalAPI demo project."
+    //        }
+    //    ],
+    //    "username": "peter123",
+    //    "firstName": "Peter",
+    //    "lastName": "Petrov"
+    //}
+}
