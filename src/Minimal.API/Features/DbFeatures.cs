e664739dc391ff97f78c88697bed94f06c4bfc61
mikegsCoder <mikegsid@gmail.com>
2024-11-26 07:18:57 +0200

    Add categories endpoint

--- src/Minimal.API/Features/DbFeatures.cs
+++ src/Minimal.API/Features/DbFeatures.cs
@@ -74,6 +74,39 @@ namespace Minimal.API.Features
             //        "description": "Create MinimalAPI demo project."
             //    }
             //]
+
+            infoGroup.MapGet("categories", async ([FromServices] IDbService db, CancellationToken cancellationToken) => await db.GetAllCategoriesAsync(cancellationToken));
+            // http://localhost:5000/db/categories
+            //[
+            //    {
+            //        "id": "43e674b8-ae0e-48a0-a231-e1bc5cf85e9c",
+            //        "name": "Other",
+            //        "tasks": []
+            //    },
+            //    {
+            //        "id": "4a9861e0-7884-4d4c-9080-b48de9c883ac",
+            //        "name": "Family",
+            //        "tasks": []
+            //    },
+            //    {
+            //        "id": "669fe84c-19ce-41f9-ba64-2c844d2b851b",
+            //        "name": "Personal",
+            //        "tasks": [
+            //            {
+            //                "id": "23bf9c00-056c-4c72-9fa7-396c28da66c7",
+            //                "category": "Personal",
+            //                "status": "In Progress",
+            //                "user": "peter123",
+            //                "description": "Create MinimalAPI demo project."
+            //            }
+            //        ]
+            //    },
+            //    {
+            //        "id": "9c24c7ac-b86f-4a30-a0fd-bb3ef6e76308",
+            //        "name": "Job",
+            //        "tasks": []
+            //    }
+            //]
         }
     }
 }
